7c205dde0dd24d95f43853700e68d36390a70d33
nickmwangemi <nickmwangemi@gmail.com>
2020-11-26 22:16:14 +0300

    Add library constructor and util methods

---
+++ main.js
@@ -0,0 +1,20 @@
+let myLibrary = []
+
+function Book(title, author, pages, read) {
+	this.title = title
+	this.author = author
+	this.pages = pages
+	this.read = read
+}
+
+Book.prototype.info = function () {
+	return `${this.title} by ${this.author}, ${this.pages} pages, ${
+		this.read ? 'read.' : 'not read yet.'
+	}`
+}
+
+Book.prototype.addBookToLibrary = function (book) {
+	book = new Book(this.title, this.author, this.pages, this.read)
+	myLibrary.push(book)
+	return `${book.title} has been added to the library.`
+}
